2ae5e9d7f1495c368447158359c2e0227677e239
Brendan Oxford <oxfordbr@gmail.com>
2013-07-29 22:27:05 -0400

    Index and Stylesheet
    Added the Index page and the stylesheet for the project.

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>180 Projects :: Home</title>
+	<link href="css/styles.css" type="text/css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Titan+One' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div id="container">
+		<div id="header">
+			<h1>180 Websites - 180 Days<h1>
+		</div>
+		<div id="nav">
+			<ul>
+				<li><a href="http://180project.brendanoxford.com/">Home</a></li>
+				<li><a href="http://180sites.brendanoxford.com/">Blog</a></li>
+				<li><a href="https://github.com/oxfordbr/180Sites">Github Repo</a></li>
+			</ul>
+		</div>
+		<div id="content">
+			<h2>The Projects</h2>
+			<h3>Day 1</h3>
+			<p>You're looking at it.</p>
+		</div>
+		<div id="footer">
+			<p>&copy; <?php echo date('Y'); ?> - Brendan Oxford <a href="http://www.brendanoxford.com">www.brendanoxford.com</a></p>
+		</div>
+	</div>
+
+</body>
+</html>
