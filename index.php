0159e072ff4c953ec34733380b2adb5bb96e2b3f
Brendan Oxford <oxfordbr@gmail.com>
2014-01-17 11:32:19 +0100

    Adding introduction to index file, updating CSS.

--- index.php
+++ index.php
@@ -18,8 +18,13 @@
 			</ul>
 		</div>
 		<div id="content">
+			<h2>Introduction</h2>
+			<p>Since this page was originally supposed to showcase all my work, but isn't really doing that for a variety of reasons I haven't worked out yet, I though I'd make it a bit of an introduction instead. What is my 180 day project?</p>
+			<p>First, it is inspired by another very similar project where <a href="http://blog.jenniferdewalt.com/post/56319597560/im-learning-to-code-by-building-180-websites-in-180">Jennifer Dewalt</a> made 180 websites in 180 days. My project is similar, but not quite the same. In my first stab at this I went the same route, but after reading the responses in the link above amongst other reasons I've opted to change my tactics a bit.</p>
+			<p>In her project Jennifer set aside 180 days where she spent her day doing one project per day, with no other obligations. Having a job, wife, social life, fitness goals, etc, this isn't viable for me. Other reasons being that having one big project gives me more experience with scope and working in an environment where I need to keep track of various elements. By putting together a CMS (the main project) I am able to work on a project that has a front end and an admin area that need to interact with each other, I can use git effectively, try out test driven development, work with environment variables, use PHP (Laravel), Composer, Artisan JavaScript, Sass, AJAX, Grunt, etc.</p>
+			<p>For the time being all updates will be on the blog and github, eventually once I've done some hosting stuff I'll start sharing the demos on here again, but that is down the road. Relevant links are at the top of the page, please feel free to comment on the blog or raise issues on github if you have any suggestions, feedback, etc. I love to hear from you!</p>
 			<h2>The Projects</h2>
-			<h3>Day 16 - ???<h3>
+			<h3>Day 16 - ???</h3>
 			<p>Beginning the CMS project.</p>
 			<h3>Day 1 - Day 15</h3>
 			<p>The first project I completed is a to do list - Link removed (see blog day 15).</p>
