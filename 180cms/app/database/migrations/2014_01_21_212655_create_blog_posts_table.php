c424afe05c37e13e9b26621c651d3cfb7997e067
Brendan Oxford <oxfordbr@gmail.com>
2014-01-21 23:29:15 +0100

    Adding blog posts migration file, updating index file

---
+++ 180cms/app/database/migrations/2014_01_21_212655_create_blog_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogPostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('blog_posts', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('title');
+			$table->description('content');
+			$table->integer('user_id');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('blog_posts', function(Blueprint $table)
+		{
+			Schema::drop('blog_posts');
+		});
+	}
+
+}
